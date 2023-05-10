fa141b1d509c4073addbf309a86e0ccbabd3b14b
lamaison <joao_lamaison@hotmail.com>
2023-05-10 00:15:31 -0300

    criado populate

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const { connect, disconnect } = require('./db')
+const populateDb = require('./populate_db')
 
 async function main() {
   const client = await connect()
+
+  await populateDb(client)
 
   await disconnect(client)
 }
---
+++ src/populate_db.js
@@ -0,0 +1,40 @@
+const fs = require('fs/promises')
+
+const jsToPgType = {
+    number: "int",
+    string: "varchar(50)"
+}
+
+async function populate(client) {
+    // Lê o arquivo .json
+  const metadataText = await fs.readFile('assets/metadata.json', { encoding: 'utf8' })
+  const metadata = JSON.parse(metadataText)
+    const tableName = Object.keys(metadata)[0]
+
+  // Caso já exista uma tabela com o nome, deleta ela
+  await client.query(`DROP TABLE IF EXISTS ${tableName};`)
+
+  // CREATE TABLE INITIAL (A int, B int);
+    const columns = Object.entries(metadata[tableName]).map(([k, v]) => {
+        const type = jsToPgType[typeof v[0]]
+        return k + " " + type
+    })
+
+  await client.query(`CREATE TABLE ${tableName} (${columns.join(", ")});`)
+
+    // INSERT INTO INITIAL (A, B) values (20, 55), (20, 30);
+    const columnNames = Object.keys(metadata[tableName]).join(", ")
+
+    const tableValues = Object.values(metadata[tableName])
+
+    const values = tableValues[0].map((v, i) => {
+        const ov = tableValues.slice(1).map(p => p[i])
+        return [v].concat(ov)
+    })
+
+    const strValues = values.map(v => `(${v.join(", ")})`).join(", ")
+
+  await client.query(`INSERT INTO ${tableName} (${columnNames}) values ${strValues};`)
+}
+
+module.exports = populate
